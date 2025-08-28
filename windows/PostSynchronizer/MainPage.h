cf042d8d8f10c3aad826358e5343567c7713549a
ElPsy <jhonnysallesnoschag@hotmail.com>
2025-08-27 22:29:15 -0300

    Criação do projeto.

---
+++ windows/PostSynchronizer/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::PostSynchronizer::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::PostSynchronizer::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
